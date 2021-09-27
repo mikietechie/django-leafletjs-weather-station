57ad0e661c9e6ad433d155ad5f7a123667681859
mikietechie <mzinyoni7@outlook.com>
2021-09-27 19:13:10 +0200

    wrote tests but havent tested them!

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,86 @@
 from django.test import TestCase
+from app.models import User, Location
+from django.shortcuts import reverse
+import sys, os
 
 # Create your tests here.
+class GlobalTestCase(TestCase):
+    
+    def setUp(self):
+        self.user = User(
+            is_superuser=True,
+            is_staff=True,
+            user_permissions="mike",
+            email="mzinyoni7@gmail.com",
+        )
+        self.user.set_password("12345678")
+        self.user.save()
+        self.client.force_login(self.user)
+        self.loc1 = Location(**{
+            "title": "Fairbury",
+            "latitude": 39.300299,
+            "longitude": -94.833984,
+        })
+        self.loc2 = Location(**{
+            "title": "Cambridge",
+            "latitude": 40.258569,
+            "longitude": -100.167847
+        })
+        self.loc3 = Location(**{
+            "title": "Russel",
+            "latitude": 38.873929,
+            "longitude": -98.827515,
+        })
+    
+    ''' Test Models '''
+    def test_user_model(self):
+        self.assertIsInstance(self.user, User)
+    
+    def test_location_model(self):
+        self.loc1.save()
+        self.loc2.save()
+        self.loc3.save()
+        self.assertIsInstance(self.loc1, Location)
+        #   check count
+        self.assertEqual(Location.objects.count(), 3)
+        #   Now test the auto creation of the other fields grid_x/_y/_id data
+        self.assertIsNone(self.loc1.grid_id)
+    
+    def test_location_str(self):
+        self.assertIn(self.loc1.title, str(self.loc1))
+    
+    def test_location_weather(self):
+        self.assertIsInstance(self.loc1.weather, dict)
+    
+    def test_location_weather_now(self):
+        self.assertIsInstance(self.loc1.weather_now, dict)
+        
+    ''' Test views '''
+    def test_404_view(self):
+        response = self.client.get("non-existent-path")
+        self.assertLess(response.status_code, 400)
+        self.assertInHTML("404", response.content)
+    
+    def test_index_view_with_no_locations(self):
+        Location.objects.delete()
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertGreater(response.context.center_x, 0)
+        self.assertLess(response.context.center_y, 0)
+    
+    def test_index_view(self):
+        self.loc1.save()
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
+    ''' Test management commands '''
+    def test_setup(self):
+        python = sys.argv[0]
+        exit_code = os.system(f"{python} manage.py setup")
+        self.assertFalse(bool(exit_code))
+    
+    
+    
+        
